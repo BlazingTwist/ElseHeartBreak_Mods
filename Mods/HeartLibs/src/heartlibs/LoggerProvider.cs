5c1f3e350d0d24115837cd82e113d3e843f5f8b1
BlazingTwist <39350649+BlazingTwist@users.noreply.github.com>
2023-03-04 04:57:02 +0100

    move LoggerProvider to HeartLibs

---
+++ Mods/HeartLibs/src/heartlibs/LoggerProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using BepInEx.Logging;
+using JetBrains.Annotations;
+
+namespace HeartLibs.heartlibs {
+
+	[PublicAPI]
+	public static class LoggerProvider {
+
+		public static ManualLogSource GetLogger() {
+			MethodBase callingMethod = new StackFrame(1, false).GetMethod();
+			return Logger.CreateLogSource(callingMethod.ReflectedType?.Name + "::" + callingMethod.Name);
+		}
+
+	}
+
+}
